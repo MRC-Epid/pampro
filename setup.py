5dca64eca5b06970f6e5594c3c0a74262419965e
Ella Hutchinson <ech61@mrc-epid.cam.ac.uk>
2018-11-05 10:09:33 +0000

    timestamp anomalies fix #1

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
     maintainer = "Ella Hutchinson",
     maintainer_email = "ella.hutchinson@mrc-epid.cam.ac.uk",
     description = ("physical activity monitor processing"),
-    url = "https://gitlab.mrc-epid.cam.ac.uk/PATT/pampro",
+    url = "https://github.com/MRC-Epid/pampro",
     install_requires=['numpy', 'scipy', 'matplotlib', 'h5py', 'pandas'],
     Classifiers = [
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: Microsoft :: Windows :: Windows 7",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.6"
