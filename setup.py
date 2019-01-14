7f417bec9ebc07ce11a6d94432fd69d01144acab
Ella Hutchinson <ech61@mrc-epid.cam.ac.uk>
2019-01-14 16:02:35 +0000

    Version 0.5.1

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 setup(
     name="pampro",
     packages=["pampro"],
-    version="0.5.0",
-    author="Tom White",
+    version="0.5.1",
+    author="Tom White\Ella Hutchinson",
     maintainer="Ella Hutchinson",
     maintainer_email="ella.hutchinson@mrc-epid.cam.ac.uk",
     description=("physical activity monitor processing"),
     url="https://github.com/MRC-Epid/pampro",
-    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py', 'pandas', 'statsmodels'],
+    install_requires=['numpy>=1.14.0', 'scipy>=1.1.0', 'matplotlib>=2.2.2', 'h5py>=2.9.0', 'pandas>==0.23.0', 'statsmodels>=0.9.0'],
     Classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: Microsoft :: Windows :: Windows 7",
