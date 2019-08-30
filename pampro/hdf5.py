c3750b2ff0a883681ec6166558d4ca7ee4a429cc
Ella Hutchinson <ech61@mrc-epid.cam.ac.uk>
2019-08-30 10:04:20 +0100

    bug fix in hdf5.py

--- pampro/hdf5.py
+++ pampro/hdf5.py
@@ -302,7 +302,7 @@ def interpolate_offsets(offsets, data_length):
         except:
             pass
 
-    return full_offse
+    return full_offset
 
 
 def save(ts, output_filename, file_header=None, groups=[("Raw", ["X", "Y", "Z"])], meta_candidates=["calibrated", "frequency", "missing_value"], compression=4, data_type="float32"):
